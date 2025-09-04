d98b3d1dc47188e6090152a400082603c2a9495f
merialea <merialea5@gmail.com>
2025-09-04 18:58:09 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+console .log(random);
+
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+
+let max = Math.max(1, 5, 10, 15, 20);
 
 // Write your code here
